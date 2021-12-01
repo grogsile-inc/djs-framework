151d94f96fd7b5d6541ae2647f0daa3485ee6623
Tilman Raendchen <tilman@medallyon.me>
2021-12-01 01:36:02 +0000

    Create CronJob.js

---
+++ src/CronJob.js
@@ -0,0 +1,46 @@
+let Job;
+try
+{
+	// Try to require the optional dependency
+	Job = require("cron").CronJob;
+}
+catch (error)
+{
+	// Discard error
+}
+
+class CronJob
+{
+	get CRON_OPTIONS_DEFAULT()
+	{
+		return {
+			cronTime: null,
+			onTick: this.job,
+			context: this,
+			start: true,
+			runOnInit: false
+		};
+	}
+
+	constructor(client, cronOptions = {})
+	{
+		if (Job == null)
+			throw new Error("'cron' is not installed. Install it via 'npm install cron'.");
+
+		if (this.constructor === CronJob)
+			throw new Error("AbstractError: 'CronJob' may not be instantiated directly.");
+
+		if (cronOptions?.cronTime == null)
+			throw new Error("You must specify a 'cronTime' property with your CronJob.");
+
+		this.client = client;
+		this.job = new Job(this.CRON_OPTIONS_DEFAULT, cronOptions);
+	}
+
+	run()
+	{
+		throw new Error(`NotImplementedError: ${this.constructor.name} (${Object.getPrototypeOf(this.constructor).name}) must implement a 'run' method.`);
+	}
+}
+
+module.exports = CronJob;
