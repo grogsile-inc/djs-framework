4b2fad9d5805a8ce4a1d923fe9f8dafa4be6ad83
Tilman Raendchen <tilman@medallyon.me>
2021-12-01 03:12:35 +0000

    Update exports

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
+const index = function(dir, recursive = Infinity, ...args)
+{
+	const fs = require("fs")
+		, files = fs.readdirSync(dir)
+		, modules = {};
+
+	if ((typeof recursive) !== "number")
+	{
+		args = [ recursive ].concat(args);
+		recursive = Infinity;
+	}
+
+	for (const file of files)
+	{
+		if (file === "index.js")
+			continue;
+
+		const filePath = join(dir, file)
+			, stat = fs.statSync(filePath);
+
+		if (stat.isDirectory())
+		{
+			// 'recursive' here stands for the the amount of dirs we should travel and is controlled by decreasing with every directory call
+			if (recursive === 0)
+				continue;
+
+			modules[file] = global.index(filePath, recursive - 1, ...args);
+			continue;
+		}
+
+		if (args.length)
+			modules[file.replace(".js", "")] = new (require(filePath))(...args);
+		else
+			modules[file.replace(".js", "")] = require(filePath);
+	}
+
+	return modules;
+};
+
+const { join } = require("path");
+
 module.exports = {
-	CronJob: require("./src/CronJob.js"),
-	DiscordEvent: require("./src/DiscordEvent.js"),
-	DiscordCommand: require("./src/DiscordCommand.js")
+	commands: index(join(__dirname, "src", "commands")),
+	CronJob: require("./src/classes/CronJob.js"),
+	DiscordEvent: require("./src/classes/DiscordEvent.js"),
+	DiscordCommand: require("./src/classes/DiscordCommand.js")
 };
