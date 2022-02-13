e3dc7c17b0d5517692f0980176b82474b8a03e64
Tilman Raendchen <tilman@medallyon.me>
2022-02-13 16:06:57 +0000

    Setup interactionCreate with commands & handlers

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ module.exports = {
 };
 async function init(client)
 {
-	function updateApplicationCommands(clientId, token, testGuildId)
+	function updateApplicationCommands(clientId, token)
 	{
 		const { REST } = require("@discordjs/rest")
 			, { Routes } = require("discord-api-types/v9")
@@ -69,9 +69,8 @@ async function init(client)
 					data.push(cmd.meta.interaction);
 			}
 
+			clientId = clientId || process.env.DISCORD_ID;
 			let route = Routes.applicationCommands(clientId);
-			if (process.env.NODE_ENV?.includes("dev") && (typeof testGuildId) === "string")
-				route = Routes.applicationGuildCommands(clientId, testGuildId);
 
 			rest.put(route, { body: data })
 				.then(commands =>
@@ -90,6 +89,42 @@ async function init(client)
 		});
 	}
 
-	// TODO: Currently only works if called after the `<Client>.ready` event was received  
+	// TODO: Currently only works if called after the `<Client>.ready` event was received
+	// or if 'process.env.DISCORD_ID' is declared
 	await updateApplicationCommands(client.user?.id);
+
+	client.on("interactionCreate", async interaction =>
+	{
+		for (const handler of BaseHandler.handlers)
+		{
+			// Comparing booleans is around 86% faster than using `instanceof` to check class
+			if (!(handler.customId === interaction.customId
+				&& handler.interactionType === Constants.InteractionTypes.MESSAGE_COMPONENT
+				&& interaction.isMessageComponent()))
+				continue;
+
+			handler.handle(interaction);
+		}
+
+		if (interaction.isCommand())
+		{
+			// Find the command that corresponds with the interaction
+			const command = modules.SlashCommand.commands.find(c => c.id === interaction.commandId);
+
+			// This should never happen, but include this in case does.
+			// TODO: This does happen in the case of duplicate guild commands
+			if (command == null)
+				return console.warn(`A Slash Command (${interaction.commandName}) was executed, but no corresponding SlashCommand instance was found.`);
+
+			try
+			{
+				// Run the command
+				await command.run(interaction);
+			}
+			catch (err)
+			{
+				console.error(err);
+			}
+		}
+	});
 }
